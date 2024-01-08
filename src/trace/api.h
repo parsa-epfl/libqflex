073c1c92d4f2f163e8445c58472942f1a40cb90d
Bryan Perdrizat <bryan.perdrizat@epfl.ch>
2024-01-08 17:02:47 +0100

    feat: implement basic instruction and memory callback

--- src/trace/api.h
+++ src/trace/api.h
@@ -1,17 +1,42 @@
+#include <stdint.h>
+#include <stddef.h>
 
+typedef enum {} branch_type_t;
+typedef enum {} translation_type_t;
 
-struct memory_transaction
+typedef struct 
 {
-    /* data */
+    size_t target_va_pc;
+    size_t target_pa_pc; // Require qemu_plugin_get_gpaddr addition to the qemu-plugin.h api, really needed ??
+
+    uint32_t opcode;
+    size_t instruction_bytes_size;
+
+    bool is_userland; // Require qemu_plugin_is_userland addition to the qemu-plugin.h api, really needed ??
+
+    translation_type_t type;
+
+
+
+} transaction_state_t;
+
+struct memory_transaction_state
+{
+    transaction_state_t transaction;
+
+    uint64_t va_page;
+    uint64_t pa_page;
+
+    bool is_io;
+    bool is_store;
 };
 
-struct transaction
+struct instr_transaction_state
 {
-    // pc
-    // physical address
+    transaction_state_t transaction;
+    branch_type_t branch_type;
 };
 
 // Define a trace callback
 //! Should not use void* but a defined type
-typedef void (*qflex_trace_cb_t) (void* userdata, void* cb);
-
+// typedef void (*qflex_trace_cb_t) (void* userdata, void* cb);
--- src/trace/qemu_plugin.c
+++ src/trace/qemu_plugin.c
@@ -1,5 +1,7 @@
+#include <glib.h>
 #include <stdint.h>
 #include <qemu-plugin.h>
+#include "api.h"
 
 
 // Ensure that the plugin run only against the version
@@ -8,12 +10,51 @@ QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
 
 
 static qemu_plugin_vcpu_mem_cb_t
-dispatch_memory_access(unsigned int vcpu_index, qemu_plugin_meminfo_t info, uint64_t vaddr, void* userdata)
-{}
+dispatch_memory_access(unsigned int vcpu_index, qemu_plugin_meminfo_t info, uint64_t vaddr, transaction_state_t* userdata)
+{
+
+
+
+    struct qemu_plugin_hwaddr* hwaddr = qemu_plugin_get_hwaddr(info, vaddr);
+
+    struct memory_transaction_state mem = {0};
+
+    //! missing PC physical address
+    mem.transaction.target_va_pc = userdata->target_va_pc;
+    mem.transaction.instruction_bytes_size = userdata->instruction_bytes_size;
+    mem.transaction.opcode = userdata->opcode;
+    //? mem.transaction.type = SMTH
+    
+    
+    mem.va_page = vaddr;
+    mem.pa_page = qemu_plugin_hwaddr_phys_addr(hwaddr);
+    mem.is_store = qemu_plugin_mem_is_store(info);
+    mem.is_io = (hwaddr && qemu_plugin_hwaddr_is_io(hwaddr)); //? Works only for full system emulation
+
+
+    g_free(userdata);
+    qemu_plugin_outs("dispatch_memory_access")
+    // Call QFLEX
+
+}
 
 static qemu_plugin_vcpu_udata_cb_t
-dispatch_instruction(unsigned int vcpu_index, void* userdata)
-{}
+dispatch_instruction(unsigned int vcpu_index, transaction_state_t* userdata)
+{
+
+    struct instr_transaction_state instr = {0};
+
+    instr.transaction.target_va_pc = userdata->target_va_pc;
+    instr.transaction.instruction_bytes_size = userdata->instruction_bytes_size;
+    instr.transaction.opcode = userdata->opcode;
+
+    //TODO, maybe ?? instr.branch_type = 
+
+
+    g_free(userdata);
+    qemu_plugin_outs("dispatch_instruction");
+
+}
 
 /**
  * Get called on every instruction translation
@@ -28,18 +69,23 @@ dispatch_vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb* tb)
     {
         struct qemu_plugin_insn* instruction = qemu_plugin_tb_get_insn(tb, i);
 
+        transaction_state_t* transaction = g_new0(transaction_state_t, 1); 
+        transaction.target_va_pc    = qemu_plugin_insn_vaddr(instruction);
+        transaction.opcode          = *(uint32_t*) qemu_plugin_insn_haddr(instruction); //? From the official plugins
+        transaction.instruction_bytes_size = qemu_plugin_insn_size(instruction);
+
         qemu_plugin_register_vcpu_mem_cb(
             instruction, 
             dispatch_memory_access, 
             QEMU_PLUGIN_CB_NO_REGS,
             QEMU_PLUGIN_MEM_RW, 
-            NULL);
+            transaction);
 
         qemu_plugin_register_vcpu_insn_exec_cb(
             instruction,
             dispatch_instruction, 
             QEMU_PLUGIN_CB_NO_REGS ,
-            NULL);
+            transaction);
     } 
 }
 
@@ -50,12 +96,11 @@ exit_plugin(qemu_plugin_id_t id, void* p)
     qemu_plugin_outs("==> TRACE END");
 }
 
-// Step 1 get a list of the event
-// PC
-// MEM access (physical)
-// list of event for specific instruction executed
-// function to access main memory
 
+/**
+ * Plugin entry. Parse the arguments. Register the call back for each transaction, 
+ * as well as plugin exit.
+ */
 QEMU_PLUGIN_EXPORT int 
 qemu_plugin_install(qemu_plugin_id_t id , const qemu_info_t* info, int argc, char** argv)
 {
