140fa5d2ef1ee6d6899526cae9d90132a685a106
Bryan Perdrizat <bryan.perdrizat@epfl.ch>
2024-05-08 08:38:10 +0200

    hmp: add flexus-save-ckpt
    Trigger Flexus to dump all the checkpoint,
    from a QEMU hmp command.

--- libqflex/libqflex-hmp-cmds.c
+++ libqflex/libqflex-hmp-cmds.c
@@ -31,5 +31,23 @@ hmp_flexus_save_measure(Monitor *mon, const QDict *qdict)
     hmp_handle_error(mon, err);
 }
 
+void
+hmp_flexus_save_ckpt(Monitor* mon, const QDict* qdict)
+{
+    if (! qemu_libqflex_state.is_configured)
+    {
+        monitor_printf(mon, "QFlex seems to not be initialised, saving Flexus make then no sense\n");
+        return;
+    }
+
+    Error    *err           = NULL;
+    int saved_vm_running = runstate_is_running();
+    vm_stop(RUN_STATE_SAVE_VM);
+
+    flexus_api.qmp(QMP_FLEXUS_DOSAVE, "./flexus_ckpt");
+
+    if (saved_vm_running)
+        vm_start();
+
     hmp_handle_error(mon, err);
 }
