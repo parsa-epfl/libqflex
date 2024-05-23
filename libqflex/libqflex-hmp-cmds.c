64e5c302ef344824f5caecb628e8dd114c881e65
Bryan Perdrizat <bryan.perdrizat@epfl.ch>
2024-05-23 14:22:21 +0200

    hmp: add flexus-load-ckpt

--- libqflex/libqflex-hmp-cmds.c
+++ libqflex/libqflex-hmp-cmds.c
@@ -15,8 +15,7 @@
 extern struct libqflex_state_t qemu_libqflex_state;
 
 void
-hmp_flexus_save_measure(Monitor *mon, const QDict *qdict)
-{
+hmp_flexus_save_measure(Monitor *mon, const QDict *qdict) {
     if (! qemu_libqflex_state.is_configured)
     {
         monitor_printf(mon, "Please activate `libqflex' to use flexus QMP commands.\n");
@@ -36,7 +35,7 @@ hmp_flexus_save_ckpt(Monitor* mon, const QDict* qdict)
 {
     if (! qemu_libqflex_state.is_configured)
     {
-        monitor_printf(mon, "QFlex seems to not be initialised, saving Flexus make then no sense\n");
+        monitor_printf(mon, "qflex seems to not be initialised, saving flexus make then no sense\n");
         return;
     }
 
@@ -45,6 +44,30 @@ hmp_flexus_save_ckpt(Monitor* mon, const QDict* qdict)
     vm_stop(RUN_STATE_SAVE_VM);
 
     flexus_api.qmp(QMP_FLEXUS_DOSAVE, "./flexus_ckpt");
+
+    if (saved_vm_running)
+        vm_start();
+
+    hmp_handle_error(mon, err);
+}
+
+void
+hmp_flexus_load_ckpt(Monitor* mon, const QDict* qdict)
+{
+    if (! qemu_libqflex_state.is_configured)
+    {
+        monitor_printf(mon, "qflex seems to not be initialised, loading to flexus make then no sense\n");
+        return;
+    }
+
+    Error    *err           = NULL;
+    int saved_vm_running = runstate_is_running();
+    vm_stop(RUN_STATE_SAVE_VM);
+
+    // TODO: Check that the directory exists
+    const char *folder_name = qdict_get_try_str(qdict, "dirname");
+
+    flexus_api.qmp(QMP_FLEXUS_DOLOAD, folder_name);
 
     if (saved_vm_running)
         vm_start();
