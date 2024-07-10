73825a18d325b0f248e350bbff70b90ae194e2dd
Orhan Unuvar <orhan.unuvar@epfl.ch>
2024-07-10 21:29:41 +0200

    add: instruction bitmask testing utility v2

--- testing/testing-utils.cc
+++ testing/testing-utils.cc
@@ -10,60 +10,47 @@
 #include <initializer_list>
 #include <numeric>
 
-template <size_t N>
-class Bitmask {
+bool isBit(char c) {
+  return c == '0' || c == '1' || c == 'x';
+}
+
+bool isBitmask(const std::string& str) {
+  for (const auto& c : str) {
+    if (!isBit(c)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+class Field {
   public:
-    using iterator_category = std::input_iterator_tag;
-    using value_type = uint32_t;
-    using difference_type = std::ptrdiff_t;
-    using pointer = uint32_t*;
-    using reference = uint32_t&;
-
-    std::bitset<N> bits{0};
-    std::bitset<N> dontCares{0};
-
-    Bitmask(const std::string& bitstring) {
-      assert(bitstring.size() == N && "String argument size does not match the bitmask size.");
-
-      for (int i = 0; i < N; ++i) {
-        if (bitstring[i] == '1') {
-          bits.set(N - 1 - i);
-        } else if (bitstring[i] == '0') {
-          bits.reset(N - 1 - i);
-        } else if (bitstring[i] == 'x' || bitstring[i] == 'X') {
-          dontCares.set(N - 1 - i);
-        }
-      }
+    Field(const std::string& str) : bitmask(str) {
+      assert(isBitmask(str) && "Invalid bitmask string.");
     }
 
-    template <size_t M>
-    Bitmask<N + M> operator+(const Bitmask<M>& other) const {
-      std::string combinedBits = bits.to_string() + other.bits.to_string();
-      std::string combinedDontCares = dontCares.to_string() + other.dontCares.to_string();
-
-      Bitmask<N + M> result(combinedBits);
-      result.dontCares = std::bitset<N + M>(combinedDontCares);
-
-      return result;
+    Field& operator=(const std::string& bits) {
+      bitmask = bits;
+      return *this;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Bitmask& bitmask) {
-      for (int i = 0; i < N; ++i) {
-        if (bitmask.dontCares[N - 1 - i]) {
-          os << 'x';
-        } else {
-          os << bitmask.bits[N - 1 - i];
-        }
-      }
-      return os;
-    }
+    std::string bitmask;
+};
 
-    class BitmaskIterator {
+struct Instruction {
+  public:
+    class InstructionIterator {
       public:
-        BitmaskIterator(const Bitmask& bitmask, bool end = false) : bitmask(bitmask) {
-          if (bitmask.dontCares.count() <= 20) {
+        InstructionIterator(const Instruction& instruction, bool end = false) : instruction(instruction) {
+          for (const auto& field : instruction.fields) {
+            bitmask += field->bitmask;
+          }
+
+          int numDontCares = std::count(bitmask.begin(), bitmask.end(), 'x');
+          
+          if (numDontCares <= 20) {
             brute = true;
-            maxIter = 1 << bitmask.dontCares.count();
+            maxIter = 1 << numDontCares;
           } else {
             brute = false;
             maxIter = 1 << 20;
@@ -76,36 +63,39 @@ class Bitmask {
           }
         }
 
-        BitmaskIterator& operator++() {
+        InstructionIterator& operator++() {
           currIter++;
           return *this;
         }
 
         uint32_t operator*() const {
-          std::bitset<N> result = bitmask.bits;
-          if (brute) {
-            for (int i = 0, j = 0; i < N; ++i) {
-              if (bitmask.dontCares[i]) {
-                result.set(i, (currIter >> j) & 1);
-                ++j;
+          uint32_t result = 0;
+
+          int N = bitmask.size();
+
+          for (int i = 0, j = 0; i < N; ++i) {
+            if (bitmask[N - 1 - i] == 'x') {
+              if (brute) { 
+                result |= ((currIter >> j) & 1) << i;
+              } else {
+                result |= dist(rand_eng) << i;
               }
-            }
-          } else {
-            for (int i = 0; i < N; ++i) {
-              if (bitmask.dontCares[i]) {
-                result.set(i, dist(rand_eng));
-              }
+              ++j;
+            } else if (bitmask[N - 1 - i] == '1') {
+              result |= 1 << i;
             }
           }
-          return (uint32_t)result.to_ulong();
+
+          return result;
         }
 
-        bool operator!=(const BitmaskIterator& other) const {
+        bool operator!=(const InstructionIterator& other) const {
             return currIter != other.currIter;
         }
 
       private:
-        const Bitmask& bitmask;
+        const Instruction& instruction;
+        std::string bitmask{""};
         int currIter;
         int maxIter;
         bool brute;
@@ -113,30 +103,49 @@ class Bitmask {
         mutable std::uniform_int_distribution<int> dist{0, 1};
     };
 
-    BitmaskIterator begin() const {
-      return BitmaskIterator(*this);
+  public:
+    template<typename... Args>
+    Instruction(Args&&... args) {
+      addFields(std::forward<Args>(args)...);
     }
 
-    BitmaskIterator end() const {
-      return BitmaskIterator(*this, true);
+    std::vector<Field*> fields;
+
+    InstructionIterator begin() const {
+      return InstructionIterator(*this);
+    }
+
+    InstructionIterator end() const {
+      return InstructionIterator(*this, true);
+    }
+
+  private:
+    template<typename First, typename... Rest>
+    void addFields(First&& first, Rest&&... rest) {
+      addField(std::forward<First>(first));
+      addFields(std::forward<Rest>(rest)...);
+    }
+
+    void addFields() {}
+
+    void addField(std::reference_wrapper<Field> field) {
+      fields.push_back(&field.get());
+    }
+
+    void addField(const std::string& str) {
+      assert(isBitmask(str) && "Invalid bitmask string.");
+      fields.push_back(new Field(str));
     }
 };
 
 int main() {
-    Bitmask<4> b1("110x");
-    Bitmask<6> b2("1x10x1");
+    Field b1("110x");
+    Field b2("1x10x1");
 
-    std::cout << "Bitmask 1: " << b1 << "\n";
-    std::cout << "Bitmask 2: " << b2 << "\n";
+    Instruction instr(b1, b2);
 
-    auto result = b1 + b2;
-
-    std::cout << "\nConcatenated Bitmask:\n";
-    std::cout << result << "\n";
-
-    std::cout << "\nStream of randomized numbers for the combined Bitmask:\n";
-    for (const auto& bm : result) {
-        std::cout << bm << "\n";
+    for (const auto& i : instr) {
+      std::cout << i << std::endl;
     }
 
     return 0;
